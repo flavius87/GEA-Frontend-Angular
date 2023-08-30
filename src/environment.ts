d3b280fbe01ee933a76cc00ab2b7089af1a6d938
Flavio Scheck <flavio.scheck@gmail.com>
2023-08-30 18:04:51 -0300

    ok

--- src/environment.ts
+++ src/environment.ts
@@ -1,5 +1,16 @@
 export const environment = {
+
   production: true,
-  Url: "https://gearquitectura.com.ar/php/obras.php"
+  //Url: "https://gearquitectura.com.ar/php/obras.php",
+  Url: "http://localhost/geabackend/obras.php",
+  contentful: {
+    spaceId: 'nyyv0igjt9lo',
+    accessToken: 'iiT-bkrRh-EE7dGzsG-C6_3u0ZXzjS3uUzNeoxeY4nc',
+    contentTypeIds: {
+      BlogCategory: 'blogCategory',
+      BlogPost: 'blogPost'
+    }
+  }
+
 };
 
